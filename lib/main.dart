05e8565e603985fcb9d312d7f1cb11fa46795304
Alice Lindsay <beamgirl490@gmail.com>
2026-04-01 11:12:25 -0400

    Initialized the firebase app so when running main an error about the app not being initialized no longer pops up. Also I added sudo code explaining the code in colors.util

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+//import 'firebase_options.dart';
 import 'app.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   try {
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform
+    );
   } catch (e) {
     debugPrint('Firebase initialization failed: $e');
   }
--- lib/utils/color_utils.dart
+++ lib/utils/color_utils.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 
+//This function recieves the amount of effort the user
+//has entered on their drawing entry and then to account for the possibility
+//that the string entered (if any) has any capital letters the case is swapped
+//to lower so all characters have the same case to match the strings of the 
+//test statments for each case
+
+//It then asigns a color to each case (default if nothing was given, high effort
+// medium effort, or low effort)
+//In this context effort means how much the user thought they tried on making
+//the drawing
 Color getEffortColor(String effort) {
-  switch (effort.toLowerCase()) {
+  switch (effort.toLowerCase()) { 
     case 'high':
       return Colors.redAccent;
     case 'medium':
