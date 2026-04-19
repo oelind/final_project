fa3bcdd8bd5b0a49c59bd89df67476dd6f420a4a
Alice Lindsay <beamgirl490@gmail.com>
2026-04-19 19:15:26 -0400

    added comments to code explaining what it does (it signs the suser out)

--- lib/services/signout_user.dart
+++ lib/services/signout_user.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-
+//This file allows the user to sign out of their account
 Future<void> signoutUser(FirebaseAuth auth) async {
   await auth.signOut();
 }
