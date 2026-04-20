52f4a860affb3a26f5f9117090dd84f3dba9e305
Alice Lindsay <beamgirl490@gmail.com>
2026-04-20 10:13:19 -0400

    Started to add comments and removed the firestore data. I should potentially add prompts to generate more tests to attempt to find other corner cases

--- test/requirement_6_test.dart
+++ test/requirement_6_test.dart
@@ -2,21 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:final_project/home_screen.dart';
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
-import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 
 void main() {
   testWidgets('Requirement 6: Buttons on home screen and settings dropdown', (WidgetTester tester) async {
     final user = MockUser(uid: 'test_uid');
     final mockAuth = MockFirebaseAuth(mockUser: user, signedIn: true);
-    final mockFirestore = FakeFirebaseFirestore();
+    
 
-    await tester.pumpWidget(MaterialApp(home: HomeScreen(auth: mockAuth, firestore: mockFirestore)));
+    await tester.pumpWidget(MaterialApp(home: HomeScreen(auth: mockAuth)));
     await tester.pumpAndSettle();
 
-    // Floating Action Button exists
+    // Floating Action Button
     expect(find.byType(FloatingActionButton), findsOneWidget);
-    // Icons.add might exist twice (FAB and "Record first entry" button)
-    expect(find.byIcon(Icons.add), findsAtLeast(1));
+    expect(find.byIcon(Icons.add), findsOneWidget);
 
     // Settings Button
     expect(find.byIcon(Icons.settings), findsAtLeast(1));
@@ -24,7 +22,7 @@ void main() {
     await tester.tap(find.byIcon(Icons.settings).first);
     await tester.pumpAndSettle();
 
-    // Dropdown items
+    // Dropdown items -- tests to check the drop down menu works
     expect(find.text('Edit Goal'), findsOneWidget);
     expect(find.text('Notification Settings'), findsOneWidget);
     expect(find.text('Sign Out'), findsOneWidget);
